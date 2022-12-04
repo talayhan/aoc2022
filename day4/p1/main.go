e40cf3ab492ba9f1013618bc9a934294a141bd49
talayhan <samet.talayhan@gmail.com>
2022-12-04 09:19:10 +0300

    Add day4

---
+++ day4/p1/main.go
@@ -0,0 +1,50 @@
+package main
+
+import (
+	"fmt"
+	"io"
+	"os"
+
+	mapset "github.com/deckarep/golang-set/v2"
+)
+
+func main() {
+	file, err := os.Open("../input.txt")
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+
+	//elves pair
+	var x1, x2, x3, x4 int
+	//the number of pairs matched the rule
+	var count int
+	for {
+		_, err := fmt.Fscanf(file, "%d-%d,%d-%d\n", &x1, &x2, &x3, &x4)
+		if err != nil {
+			if err == io.EOF {
+				break
+			}
+			fmt.Println(err)
+			os.Exit(1)
+		}
+		firstPair := mapset.NewSet[int]()
+		for i := x1; i <= x2; i++ {
+			firstPair.Add(i)
+		}
+
+		secondPair := mapset.NewSet[int]()
+		for i := x3; i <= x4; i++ {
+			secondPair.Add(i)
+		}
+
+		isSubset := secondPair.IsSubset(firstPair)
+		if !isSubset {
+			isSubset = firstPair.IsSubset(secondPair)
+		}
+		if isSubset {
+			count++
+		}
+	}
+	fmt.Println("The number of pairs does one range fully contain the other is ", count)
+}
