8854eb15f745717b4ef481dc60d9df455ad3070d
talayhan <samet.talayhan@gmail.com>
2022-12-10 23:56:09 +0300

    Add day6 - p1

---
+++ day6/p1/go.mod
@@ -0,0 +1,3 @@
+module day6/p1
+
+go 1.19
---
+++ day6/p1/main.go
@@ -0,0 +1,47 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"log"
+	"os"
+	"strings"
+)
+
+func isUnique(str string) (res bool) {
+	res = true
+
+	for i := 0; i < len(str)-1; i++ {
+		rest := str[i+1:]
+
+		index := strings.Index(rest, string(str[i]))
+		if index != -1 {
+			res = false
+			break
+		}
+	}
+
+	return res
+}
+
+func main() {
+	file, err := os.Open("../input.txt")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer file.Close()
+
+	scanner := bufio.NewScanner(file)
+
+	for scanner.Scan() {
+		line := scanner.Text()
+
+		for i := 0; i < len(line); i++ {
+			fourChar := line[i : i+4]
+			if isUnique(fourChar) {
+				fmt.Println("Marker start: ", fourChar, " i+4: ", i+4)
+				break
+			}
+		}
+	}
+}
